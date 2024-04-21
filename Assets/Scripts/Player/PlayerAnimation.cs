5db88bb092352a7e769e94dcbeefa3e7d6afb28d
za4emmne <iluhaha@mail.ru>
2024-04-21 22:39:39 +0500

    ver 1.2.9.1

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(Animator))]
 
@@ -12,10 +13,13 @@ public class PlayerAnimation : MonoBehaviour
     [SerializeField] private PlayerMovenment _playerMovenment;
     [SerializeField] private PlayerAttack _playerAttack;
 
-   private Animator _animator;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    private Coroutine _coroutine;
 
     private void Start()
     {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
     }
 
@@ -46,5 +50,27 @@ public class PlayerAnimation : MonoBehaviour
     public void OnRunAnimation()
     {
         _animator.SetFloat(AnimationNameRun, Mathf.Abs(_playerMovenment.VerticalMove + _playerMovenment.HorizontalMove));
+    }
+
+    public void OnGetDamageAnimation()
+    {
+        _coroutine = StartCoroutine(IAnimateHit());
+    }
+
+    public void Stop()
+    {
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+    }
+
+    private IEnumerator IAnimateHit()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            _spriteRenderer.color = new Color(_spriteRenderer.color.r, _spriteRenderer.color.g, _spriteRenderer.color.b, 0f);
+            yield return new WaitForSeconds(.1f);
+            _spriteRenderer.color = new Color(_spriteRenderer.color.r, _spriteRenderer.color.g, _spriteRenderer.color.b, 1f);
+            yield return new WaitForSeconds(.1f);
+        }
     }
 }
--- Assets/Scripts/Player/VampireSkill.cs
+++ Assets/Scripts/Player/VampireSkill.cs
@@ -1,19 +1,32 @@
 using UnityEngine;
+using System.Collections;
 
 public class VampireSkill : MonoBehaviour
 {
-    [SerializeField] private int _vampireIndex = 1;
+    [SerializeField] private int _vampireIndex;
+    [SerializeField] private CharactersHealth _health;
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private int _delay = 100;
+    private Coroutine _coroutine;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.TryGetComponent<Enemy>(out Enemy enemy))
+        if (collision.TryGetComponent<Enemy>(out Enemy enemy) && enemy != null)
         {
-            if (Input.GetKeyDown(KeyCode.Z))
-            {
-                Debug.Log("vampire");
-                enemy.GetComponent<CharactersHealth>().TakeDamage(_vampireIndex);
-            }
+            if(Input.GetKey(KeyCode.Z))
+            _coroutine = StartCoroutine(Vampired(enemy));
+        }
+    }
 
+    private IEnumerator Vampired(Enemy enemy)
+    {
+        var waitForAnySecond = new WaitForSeconds(_delay);
+
+        for (int i = 0; i < 3; i++)
+        {
+            enemy.GetComponent<CharactersHealth>().TakeDamage(_vampireIndex);
+            _health.TakePills(_vampireIndex);
+            yield return new WaitForSeconds(1f);
         }
     }
 }
