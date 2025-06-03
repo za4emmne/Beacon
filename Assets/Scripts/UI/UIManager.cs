19490cc4fd494831bfa04e908f8d8efc126dcc1f
za4emmne <iluhaha@mail.ru>
2025-06-03 21:51:59 +0500

    ver 1.3.3
    add settting button

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -8,10 +8,28 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Text _levelText;
     [SerializeField] private GameObject[] _gameOverScreen;
     [SerializeField] private PlayerProgress _player;
+    [SerializeField] private GameObject _settingButton;
+
+    private ButtonManager _settingButtonManager;
+
+    private void Awake()
+    {
+        _settingButtonManager = _settingButton.GetComponent<ButtonManager>();
+    }
 
     private void Start()
     {
         ScreenManage(_gameOverScreen, false);
+    }
+
+    private void OnEnable()
+    {
+        _settingButtonManager.OnShowTooltip += ShowSettingButtonAnimation;
+    }
+
+    private void OnDisable()
+    {
+        _settingButtonManager.OnShowTooltip -= ShowSettingButtonAnimation;
     }
 
     public void ChangeScore(int Score)
@@ -35,5 +53,11 @@ public class UIManager : MonoBehaviour
         {
             ui.SetActive(status);
         }
+    }
+
+    private void ShowSettingButtonAnimation() //доделать
+    {
+        _settingButton.transform.DORotate(new Vector3(0, 0, 180), 0.5f)
+            .SetEase(Ease.InCirc);
     }
 }
